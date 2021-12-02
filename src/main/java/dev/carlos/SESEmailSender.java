895ec50121c3835d19d11854761a5302c5d45123
Carlos Herrera <cahe1540@gmail.com>
2021-12-01 23:34:43 -0800

    email refactoring

--- src/main/java/dev/carlos/SESEmailSender.java
+++ src/main/java/dev/carlos/SESEmailSender.java
@@ -9,14 +9,47 @@ import com.amazonaws.services.simpleemail.model.Content;
 import com.amazonaws.services.simpleemail.model.Destination;
 import com.amazonaws.services.simpleemail.model.Message;
 import com.amazonaws.services.simpleemail.model.SendEmailRequest;
+import java.util.Objects;
 
 public class SESEmailSender {
+
   public static void sendEmail(Email email) throws IOException {
+    if (Objects.isNull(email.getConfigSet())) {
+      sendEmailWithoutConfig(email);
+    }
+    sendEmailWithConfig(email);
+  }
+
+  private static void sendEmailWithConfig(Email email) throws IOException {
     try {
       AmazonSimpleEmailService client =
           AmazonSimpleEmailServiceClientBuilder.standard()
-              // Replace US_WEST_2 with the AWS Region you're using for
-              // Amazon SES.
+              .withRegion(email.getAwsregion()).build();
+      SendEmailRequest request = new SendEmailRequest()
+          .withDestination(
+              new Destination().withToAddresses(email.getTo()))
+          .withMessage(new Message()
+              .withBody(new Body()
+                  .withHtml(new Content()
+                      .withCharset("UTF-8").withData(email.getHtmlBody()))
+                  .withText(new Content()
+                      .withCharset("UTF-8").withData(email.getTextBody())))
+              .withSubject(new Content()
+                  .withCharset("UTF-8").withData(email.getSubject())))
+          .withSource(email.getFrom())
+          .withConfigurationSetName(email.getConfigSet());
+      client.sendEmail(request);
+      System.out.println("Email sent!");
+    } catch (Exception ex) {
+      System.out.println("The email was not sent. Error message: "
+          + ex.getMessage());
+    }
+  }
+
+  private static void sendEmailWithoutConfig(Email email) throws IOException {
+    try {
+      AmazonSimpleEmailService client =
+          AmazonSimpleEmailServiceClientBuilder.standard()
               .withRegion(email.getAwsregion()).build();
       SendEmailRequest request = new SendEmailRequest()
           .withDestination(
@@ -30,9 +63,6 @@ public class SESEmailSender {
               .withSubject(new Content()
                   .withCharset("UTF-8").withData(email.getSubject())))
           .withSource(email.getFrom());
-      // Comment or remove the next line if you are not using a
-      // configuration set
-      //.withConfigurationSetName(CONFIGSET);
       client.sendEmail(request);
       System.out.println("Email sent!");
     } catch (Exception ex) {
@@ -41,7 +71,7 @@ public class SESEmailSender {
     }
   }
 
-  public static String prepareBody(String arg1, String arg2, String html){
+  public static String prepareBody(String arg1, String arg2, String html) {
     html = html.replace("<NAME>", arg1);
     html = html.replace("<MESSAGE>", arg2);
     return html;
